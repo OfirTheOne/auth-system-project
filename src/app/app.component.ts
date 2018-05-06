ccd52b466a99e9200d12e538764a2f32c02e7ade
TheCodeSleyer <ofir3322@gmail.com>
2018-05-06 10:40:01 +0300

    inject env variables bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,9 @@ export class MyApp {
   rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    //console.log(ENV)
+    console.log(ENV);
     platform.ready().then(() => {
-      
+      console.log(ENV);
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
