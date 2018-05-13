0a45713be3443d630344580893c9d6f960b39414
TheCodeSleyer <ofir3322@gmail.com>
2018-05-13 20:35:16 +0300

    configure the env var webpack.config

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
 });
 
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
