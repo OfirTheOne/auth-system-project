058bd90c1c82b145a0942ee6823ffe411b221888
TheCodeSleyer <ofir3322@gmail.com>
2018-05-13 18:39:20 +0300

    configure the env var webpack.config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
-
+console.log(JSON.stringify(process.argv, undefined, 2));
 const resolvePathToEnvModule = (ionicEnv) => {
   let curEnvPath;
   if(ionicEnv == 'prod') {
@@ -12,12 +12,19 @@ const resolvePathToEnvModule = (ionicEnv) => {
   return curEnvPath;
 }
 
-const recoverUndefineIonicEnv = (ionicEnv) => {
-  if(!ionicEnv) {
+const recoverFailedIonicEnv = (ionicEnv) => {
+  if(process.argv.slice(2).some((arg) => arg.indexOf('--prod') !== -1)) {
+    // if the build command contains --prod parameter runover the old value of ionicEnv and set to 'prod'.
+    ionicEnv = 'prod';
+
+  } else if(!ionicEnv) {
+    // if ionicEnv undefine chack the value of NODE_ENV.
     if(process.env.NODE_ENV == 'production') {
       ionicEnv = 'prod';
+ 
     } else {
       ionicEnv = 'dev';
+
     }
   }
   return ionicEnv;
@@ -26,7 +33,7 @@ const recoverUndefineIonicEnv = (ionicEnv) => {
 
 const appScriptsDir = process.env.IONIC_APP_SCRIPTS_DIR || '@ionic/app-scripts';
 const rootDir = process.env.IONIC_ROOT_DIR;
-const ionicEnv = recoverUndefineIonicEnv(process.env.IONIC_ENV);
+const ionicEnv = recoverFailedIonicEnv(process.env.IONIC_ENV);
 
 console.log(`ionicEnv: ${ionicEnv}`);
 console.log(`process.env.IONIC_ENV: ${process.env.IONIC_ENV}`);
